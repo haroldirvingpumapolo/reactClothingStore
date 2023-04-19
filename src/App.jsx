a5cf9f93b4d14c7855e5a8a5d83dee9cd09667b0
haroldirvingpumapolo <pumapoloharold123@gmail.com>
2023-04-19 00:10:36 -0500

    Added filter by sizes with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ShowProducts from "./Components/ShowProducts";
-
+import dataArrButtons from "./Components/dataArrButtons";
+import FilterButtons from "./Components/FilterButtons";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [buttons, setButtons] = useState([]);
+  const [sizes, setSizes] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,13 +15,29 @@ function App() {
       .then((res) => {
         setProducts(res.data.products);
       });
+    setButtons(dataArrButtons);
   }, []);
+
+  function handleSizeClick(size) {
+    setButtons(
+      buttons.map((button) =>
+        button.buttonValue === size
+          ? { ...button, select: !button.select }
+          : button
+      )
+    );
+    sizes.includes(size)
+      ? setSizes(sizes.filter((s) => s !== size))
+      : setSizes([...sizes, size]);
+  }
 
   return (
     <div className="flex items-start select-none mt-24">
-      <ShowProducts
-        dataShowProducts={products}
+      <FilterButtons
+        dataButtons={buttons}
+        onHandleSizeClick={handleSizeClick}
       />
+      <ShowProducts dataShowProducts={products} dataSizes={sizes} />
     </div>
   );
 }
--- src/Components/ShowProducts.jsx
+++ src/Components/ShowProducts.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Product from "./Product";
 
-function ShowProducts({ dataShowProducts}) {
+function ShowProducts({ dataShowProducts, dataSizes }) {
   return (
     <div className="flex flex-col w-full items-center">
       <div className="flex w-full justify-start">
         <h1 className="text-2xl">16 Product(s) found</h1>
       </div>
       <div className="flex flex-row flex-wrap w-full justify-between">
-        {dataShowProducts.map((product, i) => (
+        {dataShowProducts
+          .filter(
+            (product) =>
+              dataSizes.length === 0 ||
+              dataSizes.some((size) => product.availableSizes.includes(size))
+          )
+          .map((product, i) => (
             <div key={i}>
               <Product
                 sku={product.sku}
